71305983287426151b7611edfe510299ec7d5d30
claude-perrin <vity5.diduk@gmail.com>
2022-04-01 22:48:40 +0200

    f

---
+++ Proxies/proxyhandler/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Proxies(models.Model):
+    Ip = models.CharField(max_length=15)
+    Port = models.IntegerField()
+    Success = models.IntegerField()
+    Speed = models.IntegerField("Speed in ms")
+    Latency = models.IntegerField("Latency in ms")
+    pub_date = models.DateTimeField('date published')
+    Updated = models.DateTimeField("Updated")
+
+
+
