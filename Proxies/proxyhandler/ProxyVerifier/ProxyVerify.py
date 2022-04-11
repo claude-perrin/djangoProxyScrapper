54b5606cba73d0676ef5ef3035c8af3c5f9a2da8
claude-perrin <vity5.diduk@gmail.com>
2022-04-11 23:02:44 +0200

    config file added

--- Proxies/proxyhandler/ProxyVerifier/ProxyVerify.py
+++ Proxies/proxyhandler/ProxyVerifier/ProxyVerify.py
@@ -3,17 +3,10 @@ from datetime import datetime
 from threading import Thread
 
 from tqdm import tqdm
-
+from .config import *
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
-
-
-class Configuration:
-    test_url = 'http://127.0.0.1:8000/'
-    VERIFICATION_NUMBER = 5
-    timeout = 1.6
-    user_agent = {"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"}
 
 
 class ProxyNotResponding(Exception):
@@ -21,7 +14,6 @@ class ProxyNotResponding(Exception):
 
 
 class Verification:
-    config = Configuration()
     successful_connection_counter = 0
     speed = 0
 
@@ -48,8 +40,8 @@ class Verification:
         with requests.Session() as session:
             start_time = time.perf_counter()
             try:
-                session.get(self.config.test_url, proxies=self.proxy, timeout=self.config.timeout,
-                            headers=self.config.user_agent)
+                session.get(TEST_URL, proxies=self.proxy, timeout=TIMEOUT,
+                            headers=USER_AGENT)
             except Exception as ex:
                 print(ex)
                 raise ProxyNotResponding
@@ -68,7 +60,7 @@ def verify_proxies(proxies):
     for proxy in tqdm(proxies):
         verification_obj = Verification(proxy)
         verified_proxies.append(verification_obj)
-        for _ in range(Verification.config.VERIFICATION_NUMBER):
+        for _ in range(VERIFICATION_NUMBER):
             thread = Thread(target=verification_obj.verify_proxy_connection)
             thread.start()
             threads.append(thread)
